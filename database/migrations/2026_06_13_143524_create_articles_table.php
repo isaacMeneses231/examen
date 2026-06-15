bd3961a1a4d5033786a65b12f847ebb01bea45a7
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-06-15 13:05:46 -0600

    feat: create articles table migration

---
+++ database/migrations/2026_06_13_143524_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('internal_code', 50);
+            $table->string('detailed_description', 255);
+            $table->decimal('current_sale_price', 12, 2);
+            $table->decimal('average_purchase_cost', 12, 2);
+            $table->string('availability_status', 20);
+            $table->date('entry_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
