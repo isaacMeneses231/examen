30ec102cbdc30a3dd8b9cf8e311fadf7ea3cdb80
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-06-15 13:06:38 -0600

    feat: create orders migration with foreign keys

---
+++ database/migrations/2026_06_13_143843_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+           $table->id();
+           $table->dateTime('creation_date_time');
+           $table->decimal('subtotal', 12, 2);
+           $table->decimal('tax_amount', 12, 2);
+           $table->decimal('general_total', 12, 2);
+           $table->string('additional_notes', 255);
+           $table->string('order_status', 20);
+
+           $table->unsignedBigInteger('client_id');
+           $table->unsignedBigInteger('shipping_address_id');
+
+           $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade')->onUpdate('cascade');
+           $table->foreign('shipping_address_id')->references('id')->on('shipping_addresses')->onDelete('cascade')->onUpdate('cascade');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
