ea8c1ec025b735eb213de5972528fc94ea1c348a
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-06-20 08:41:49 -0600

    refactor: update orders table with iva, order date, and explicit foreign keys

--- database/migrations/2026_06_13_143843_create_orders_table.php
+++ database/migrations/2026_06_13_143843_create_orders_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
            $table->id();
-           $table->dateTime('creation_date_time');
+           $table->dateTime('order_date');
            $table->decimal('subtotal', 12, 2);
-           $table->decimal('tax_amount', 12, 2);
+           $table->decimal('iva_amount', 12, 2);
            $table->decimal('general_total', 12, 2);
            $table->string('additional_notes', 255);
            $table->string('order_status', 20);
 
-           $table->unsignedBigInteger('client_id');
-           $table->unsignedBigInteger('shipping_address_id');
+           $table->integer('client_id')->unsigned();
+           $table->integer('shipping_address_id')->unsigned();
 
            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade')->onUpdate('cascade');
            $table->foreign('shipping_address_id')->references('id')->on('shipping_addresses')->onDelete('cascade')->onUpdate('cascade');
