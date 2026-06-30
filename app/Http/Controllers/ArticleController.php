daf114721917856ffe322eaf597f30fe34329eed
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-06-29 22:03:34 -0600

    feat: create ArticleController with resource methods

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ArticleRequest;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::orderByDesc('id')->get();
+        return view('articles.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $article = new Article();
+        return view('articles.create', compact('article'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ArticleRequest $request)
+    {
+        Article::create($request->validated());
+        return redirect()->route('articles.index')->with('success', 'Artículo creado exitosamente');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->update($request->validated());
+        return redirect()->route('articles.index')->with('success', 'Artículo actualizado exitosamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return redirect()->route('articles.index')->with('success', 'Artículo eliminado correctamente');
+    }
+}
